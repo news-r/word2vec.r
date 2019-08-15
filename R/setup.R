096d33e0d030b85c274edd65923f4d59b6b2edf8
John Coene <jcoenep@gmail.com>
2019-08-15 15:37:22 +0200

    init alias

--- R/setup.R
+++ R/setup.R
@@ -7,9 +7,14 @@
 #' @import JuliaCall
 #' @import assertthat
 #' 
+#' @name setup_word2vec
 #' @export
 setup_word2vec <- function (...){
   julia <- JuliaCall::julia_setup(...)
   JuliaCall::julia_install_package_if_needed("Word2Vec")
   JuliaCall::julia_library("Word2Vec")
 }
+
+#' @rdname setup_word2vec
+#' @export
+init_word2vec <- setup_word2vec
